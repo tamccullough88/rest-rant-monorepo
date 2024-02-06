c5cfe14fe17fc4abdefd6ab0f8eafc8f5e458f23
tamccullough88 <tamccullough88@gmail.com>
2024-02-05 21:12:10 -0500

    added JWT login ability

--- frontend/src/users/LoginForm.js
+++ frontend/src/users/LoginForm.js
@@ -18,21 +18,28 @@ function LoginForm() {
 
 
     async function handleSubmit(e) {
-        const response = await fetch(`http://localhost:5000/authentication/`, {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify(credentials)
-        })
+        e.preventDefault();
+        try {
+            const response = await fetch(`http://localhost:5000/authentication/`, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(credentials)
+            })
 
-        const data = await response.json()
+            const data = await response.json()
 
-        if (response.status === 200) {
-            setCurrentUser(data.user)
-            history.push(`/`)
-        } else {
-            setErrorMessage(data.message)
+            if (response.status === 200) {
+                setCurrentUser(data.user)
+                localStorage.setItem('token', data.token)
+                history.push(`/`)
+            } else {
+                setErrorMessage(data.message)
+            }
+        } catch (error) {
+            console.error("an error occured:", error)
+            setErrorMessage("An error occured, please try again");
         }
     }
 
